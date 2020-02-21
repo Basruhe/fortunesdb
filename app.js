1dbb305a110fd70fe9074602b0d36a18136a6d36
Basruhe <sb.ruhe@gmail.com>
2020-02-21 17:05:05 +0100

    added random get

--- app.js
+++ app.js
@@ -7,4 +7,19 @@ app.get("/fortunes", (req, res) => {
   res.send(fortunes);
 });
 
+// Get a random fortune
+app.get("/fortunes/random", (req, res) => {
+  // const random_index = Math.floor(Math.random() * fortunes.length);
+  // console.log(random_index);
+  // random_fortune = fortunes[random_index];
+  // res.json(random_fortune);
+  // // Rewritten:
+  res.json(fortunes[Math.floor(Math.random() * fortunes.length)]);
+  console.log("Request for random fortune");
+});
+
+// Get a single fortune by ID
+
 module.exports = app;
+
+// To start: nodemon ./bin/www
